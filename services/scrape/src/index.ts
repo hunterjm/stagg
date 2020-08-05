cad8078ffb4feee618d566968cebdb09265dfbb7
Dan Lindsey <dan@mdlindsey.com>
2020-08-05 12:07:02 -0400

    Scraper

--- services/scrape/src/index.ts
+++ services/scrape/src/index.ts
@@ -1,6 +1,5 @@
 import * as cors from 'cors'
 import * as express from 'express'
-import * as API from '@stagg/api'
 import * as mdb from '@stagg/mdb'
 import * as cod from './callofduty'
 import cfg from './config'
@@ -17,21 +16,7 @@ app.use(cors({ credentials: false })).listen(cfg.port, async () => {
     )
     
     // Init db connection so we don't have race condition and multiple connections
-    // let total = 19500
-    const db = await mdb.config(cfg.mongo).client('callofduty')
-    // const { auth } = await db.collection('accounts').findOne({ auth: { $exists: true } })
-    // const CallOfDutyAPI = new API.CallOfDuty(auth)
-    // const matchIds = await db.collection('mw.mp.performances').distinct('matchId')
-    // for(const matchId of matchIds.slice(total)) {
-    //     total++
-    //     console.log(`Fetching match events #${total}`)
-    //     const matchEvents = await CallOfDutyAPI.MatchEvents(matchId)
-    //     const existing = await db.collection('_raw.mw.mp.events').findOne({ matchId })
-    //     if (!existing) {
-    //         console.log(`Inserting match events for ${matchId}`)
-    //         await db.collection('_raw.mw.mp.events').insertOne(matchEvents)
-    //     }
-    // }
+    await mdb.config(cfg.mongo).client('callofduty')
     cod.initializeNewPlayers()
     cod.updateExistingPlayers()
     cod.recheckExistingPlayers()
