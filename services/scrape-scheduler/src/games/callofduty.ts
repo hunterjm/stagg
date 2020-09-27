2c5b4ed7fb3992f190f030145646181596622a4c
Dan Lindsey <dan@mdlindsey.com>
2020-09-27 17:45:52 -0400

    Rescrape end of accts on update

--- services/scrape-scheduler/src/games/callofduty.ts
+++ services/scrape-scheduler/src/games/callofduty.ts
@@ -8,19 +8,43 @@ export const triggerAll = async () => {
     for(const acct of initAccts) {
         initializeAccount(acct)
     }
-    const updateAccts = await getUpdateAccountIds()
-    for(const acct of updateAccts) {
-        updateAccount(acct)
+    const { accounts, ledgers } = await getUpdateAccountIds()
+    for(const acct of accounts) {
+        updateAccount(acct, ledgers.find(l => String(l._id) === String(acct._id)))
     }
 }
 
-export const updateAccount = (acct:Schema.DB.Account) => {
+export const updateAccount = (acct:Schema.DB.Account, ledger?:any) => {
     const accountId = acct._id
     console.log('[^] Updating', accountId, 'at', WORKER_COD_HOST)
     for(const gameId of acct.games) {
         console.log('    Game:', gameId)
         for(const gameType of ['wz', 'mp']) {
+            let oldestStart = 0
+            if (ledger && ledger[gameId] && ledger[gameId][gameType]) {
+                oldestStart = ledger[gameId][gameType]
+            }
             console.log('      Type:', gameType)
+            if (oldestStart) {
+                axios.post(WORKER_COD_HOST, {
+                    accountId,
+                    gameId,
+                    gameType,
+                    retry: 3,
+                    start: oldestStart,
+                    offset: 500,
+                    redundancy: false,
+                    delay: {
+                        success: 100,
+                        failure: 500,
+                    },
+                    include: {
+                        events: true,
+                        details: true,
+                        summary: true,
+                    },
+                }).catch(e => console.log('[!] FaaS Failure'))
+            }
             axios.post(WORKER_COD_HOST, {
                 accountId,
                 gameId,
@@ -81,12 +105,15 @@ export const getInitializeAccountIds = async ():Promise<Schema.DB.Account[]> => 
     return accounts.filter(a => !ledgerAccountIdStrs.includes(String(a._id)))
 }
 
-export const getUpdateAccountIds = async ():Promise<Schema.DB.Account[]> => {
+export const getUpdateAccountIds = async ():Promise<{ledgers: any[], accounts:Schema.DB.Account[]}> => {
     const db = await useClient('callofduty')
     const minSelectedTime = Date.now() - UPDATE_COOLDOWN
     const accountLedgers = await db.collection('_ETL.ledger').find({ selected: { $lt: minSelectedTime } }, { _id: 1 } as any).toArray()
     const validAccountIds = accountLedgers.sort((a,b) => a.selected - b.selected).map(l => l._id)
     const validAccounts = await db.collection('accounts').find({ _id: { $in: validAccountIds } }).toArray()
     const validAccountIdStrs = validAccountIds.map(_id => String(_id))
-    return validAccounts.sort((a,b) => validAccountIdStrs.indexOf(String(a._id)) - validAccountIdStrs.indexOf(String(b._id)))
+    return {
+        ledgers: accountLedgers,
+        accounts: validAccounts.sort((a,b) => validAccountIdStrs.indexOf(String(a._id)) - validAccountIdStrs.indexOf(String(b._id)))
+    }
 }
