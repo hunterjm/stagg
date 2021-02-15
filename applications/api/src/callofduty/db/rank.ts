0e48d0d7e3aa79484e7d6bdf0fd3436d9dd49ef8
Dan Lindsey <36630328+mdlindsey@users.noreply.github.com>
2021-02-15 17:26:48 -0500

    Rank fix (#45)

--- applications/api/src/callofduty/db/rank.ts
+++ applications/api/src/callofduty/db/rank.ts
@@ -1,5 +1,12 @@
 import { CONFIG } from 'src/config'
 
+const romanNumeral = (count:number) => {
+    if (count === 4) return 'IV'
+    let q = ''
+    for(let i = 1; i < count; i++) q += 'I'
+    return q
+}
+
 export const wzRank = (score:number, kills:number, deaths:number, damageDone:number, damageTaken:number) => {
     const factors = [
         { weight: CONFIG.RANKING.WZ.KDR.weight,   limitValue: CONFIG.RANKING.WZ.KDR.limit,    statValue: kills / (deaths || 1) },
@@ -15,16 +22,13 @@ export const wzRank = (score:number, kills:number, deaths:number, damageDone:num
         }
     }
     if (!factoredScores.length) {
-        return { rankId: 1, rankLabel: 'Bronze I' }
+        return { id: 1, label: 'Bronze I' }
     }
     const sum = factoredScores.reduce((a,b) => a+b)
     const avg = sum / factoredScores.length
     const rankValue = 1 + Math.max(0, Math.round(avg * (CONFIG.RANKING.tierNames.length * CONFIG.RANKING.ranksPerTier)))
-    const rankTier = Math.floor(rankValue / CONFIG.RANKING.ranksPerTier)
-    let rankQualifier = 'I'
-    for(let i = 1; i < rankValue - (rankTier * CONFIG.RANKING.ranksPerTier); i++) {
-        rankQualifier += 'I'
-    }
+    const rankTier = Math.floor((rankValue - 1) / CONFIG.RANKING.ranksPerTier)
+    const rankQualifiers = rankValue - (rankTier * CONFIG.RANKING.ranksPerTier)
 
-    return { id: rankValue, label: `${CONFIG.RANKING.tierNames[rankTier]} ${rankQualifier}` }
+    return { id: rankValue, label: `${CONFIG.RANKING.tierNames[rankTier]} ${romanNumeral(rankQualifiers)}` }
 }
